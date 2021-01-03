77c6345633a0fdefd9b0a46b341607d70fc30208
ShaliniR8 <61011528+ShaliniR8@users.noreply.github.com>
2021-01-03 20:17:05 +0530

    ran 1st test case

---
+++ testing_practice/check_test.py
@@ -0,0 +1,10 @@
+import unittest
+from check import LetterCompiler
+class TestCompiler(unittest.TestCase):
+
+    def test_basic(self):
+        testcase = "The best preparation for tomorrow is doing your best today."
+        expected = ['b', 'a', 'a', 'b', 'a']
+        self.assertEqual(LetterCompiler(testcase), expected)
+
+unittest.main()
